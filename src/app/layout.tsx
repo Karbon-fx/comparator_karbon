91178aa50190b9eee73e2f0f09146f1877c5bb33
Lakksith Hariie <lakksith@karbonbusiness.com>
2025-10-24 19:55:20 +0000

    Version 1.8 (Microsoft Clarity Deployed)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { MicrosoftClarity } from '@/components/MicrosoftClarity';
 
 export const metadata: Metadata = {
   title: 'FX Savings Ace',
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className="font-body antialiased h-full bg-transparent">
         {children}
         <Toaster />
+        <MicrosoftClarity />
       </body>
     </html>
   );
